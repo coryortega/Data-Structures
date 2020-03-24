38dd89347d8b2f818c59e9832376a7a7f48b38d6
coryortega <coryortega3@gmail.com>
2020-03-23 18:38:23 -0700

    met mvp

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -6,14 +6,24 @@ from doubly_linked_list import DoublyLinkedList
 class Queue:
     def __init__(self):
         self.size = 0
+        self.storage = DoublyLinkedList()
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
 
     def enqueue(self, value):
-        pass
+        # adding value to the beginning, or tail, of the list
+        self.storage.add_to_tail(value)
+        # incrementing size by 1 to keep track of the size
+        self.size += 1
+        return
 
     def dequeue(self):
-        pass
+        # if size is 0, then theres nothing to deque
+        if self.size == 0:
+            return
+        # if the size is greater than 0, then decrement the size attribute and remove from front of list
+        self.size -= 1
+        return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -5,14 +5,24 @@ from doubly_linked_list import DoublyLinkedList
 class Stack:
     def __init__(self):
         self.size = 0
+        self.storage = DoublyLinkedList()
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
 
     def push(self, value):
-        pass
+        # add one to our size attribute to keep track of length
+        self.size += 1
+        # add to head of list, as if we're stacking
+        self.storage.add_to_head(value)
 
     def pop(self):
-        pass
+        # if size is 0, then there's nothing to pop so just return
+        if self.size == 0:
+            return
+        # if size is greater than 0, decrement the size by 1 and remove the value from the head
+        self.size -= 1
+        return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.size
+        
